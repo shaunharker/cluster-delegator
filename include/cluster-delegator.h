f6f39b0a7b1134e6c9089e644f8dac3c8a27538e
Shaun Harker <sharker81@gmail.com>
2015-07-04 11:34:58 -0400

    fixing broken travis build

---
+++ include/cluster-delegator.h
@@ -0,0 +1,15 @@
+/// cluster-delegator.hpp
+/// Shaun Harker
+/// 2015-07-04
+
+// Note: This is the in-source version. The distribution version
+//       is an amalgamation of all the project header files.
+
+#ifndef CLUSTER_DELEGATOR_MAIN_H
+#define CLUSTER_DELEGATOR_MAIN_H
+#include "delegator/Communicator.h"
+#include "delegator/Coordinator_Worker_Process.h"
+#include "delegator/Coordinator_Worker_Scheme.h"
+#include "delegator/delegator.h"
+#include "delegator/Message.h"
+#endif
